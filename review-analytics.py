dc06e90254691a7d234c0fcc3c301a3c182a41e0
Oliver Tang <crptopochari@gmail.com>
2022-05-01 23:24:20 +0800

    Add AVG.length

--- review-analytics.py
+++ review-analytics.py
@@ -1,8 +1,16 @@
-data = [] 
+datas = [] 
 count = 0
+
+#Open files + print len + remainder filter
 with open("reviews.txt","r") as f:
 	for line in f:
-		data.append(line)
+		datas.append(line)
 		count += 1
 		if count % 10000 ==0:
-			print(len(data))
+			print(len(datas))
+
+#Avg Length
+sun_len = 0
+for data in datas:
+	sun_len += len(data)
+print("Average= ", sun_len/len(datas), " Characters each.")
