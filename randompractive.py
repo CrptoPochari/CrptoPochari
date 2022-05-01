debbec80f573cf3d733e4b463e7e19a8c60fb734
Oliver Tang <crptopochari@gmail.com>
2022-05-01 17:01:55 +0800

    add count

--- randompractive.py
+++ randompractive.py
@@ -1,12 +1,15 @@
 import random
 
 r = random.randint(1,100)
+count = 0
 while True:
+	count += 1
+	print("This is ", count ,"Times you guest.")
 	entnum = int(input("Enter your number: "))
 	if entnum == r:	
-		print("Correct")
+		print("Correct\n")
 		break
 	elif entnum < r:
-		print("Wrong, bigger please.")
+		print("Wrong, bigger please.\n")
 	else:
-		print("Wrong, smaller please.")
+		print("Wrong, smaller please.\n")
