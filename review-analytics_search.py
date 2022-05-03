0de9a5ff5af8f3e8976f833f9644a17d81ff2457
Oliver Tang <crptopochari@gmail.com>
2022-05-04 00:03:16 +0800

    Add pip installed package: progressbar and concept of object

--- review-analytics_search.py
+++ review-analytics_search.py
@@ -1,13 +1,23 @@
+import time
+import progressbar
+
+
 def Open_files(filename):
+	#.ProgressBar() is not function, is a type of object! (self define)
+	bar = progressbar.ProgressBar(max_value=1000000)
 	#Open files + print len + remainder filter
-	datas = [] 
+	datas = []
+	cnt = 0
 	with open(filename,"r") as f:
 		for line in f:
 			datas.append(line)
+			cnt += 1
+			bar.update(cnt)
 	return datas
 
 
-def word_cnt(datas):	
+def word_cnt(datas):
+	start_time = time.time()	
 	word_cnt = {}
 	# data = 1 review, datas = 100 millions review list
 	for data in datas:
@@ -22,7 +32,12 @@ def word_cnt(datas):
 	for word in word_cnt:
 		if word_cnt[word] > 10000000:
 			print(word,": ", word_cnt[word])
+	end_time = time.time()	
+	print("It spend: ", end_time - start_time, " seconds.")
+	
+	return word_cnt
 
+def search(word_cnt):	
 	while True:
 		word = input("what did you want to find?")
 		if word == "q":
@@ -33,11 +48,11 @@ def word_cnt(datas):
 		else:
 			print("Not exist!")
 
-	print("Thanks for using our search system")
+	print("Thanks for using our searching system")
 
 def main():
-	file = Open_files("reviews.txt")
-	word_cnt(file)
-
+	datas = Open_files("reviews.txt")
+	Worddic = word_cnt(datas)
+	search(Worddic)
 
 main()
