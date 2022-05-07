d411e1e810d626ea32cde7daf7d474e503730359
Wen Chun Tang <crptopochari@gmail.com>
2022-05-07 15:57:35 +0800

    os.listdir; str.endswith; Pillow: PIL.image.open ; save; file extension adjustment; replace with new extension;

--- picture_process.py
+++ picture_process.py
@@ -1,5 +1,16 @@
 from PIL import Image as img
+import os 
+# os is a module, not a package Properly speaking
 # every thing could use .fuction is an "object"
-image_f = img.open("oliverdive.jpg")  #image_f is an object
-image_f = image_f.convert('L')  #convert to black and white
-image_f.save("result.png")
+
+
+for f in os.listdir("orig"): # . means current path 
+    if f.endswith(".jpg"):
+        # f is name
+        # listdir list the name in the dir "orig"
+        #image_f = img.open("orig/" + f)  #image_f is an object
+        image_f = img.open(os.path.join("orig", f))  #image_f is an object        
+        image_f = image_f.convert('L')  #convert to black and white
+        image_f.save(os.path.join("result", f[:-4]) + "_grey.png") 
+        # endswith is str's function
+        # -4 means to the -4 position ()
