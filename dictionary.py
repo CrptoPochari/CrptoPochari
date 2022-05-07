0dde6044c167b05b8a64cde8226d0f4ad2b54d1b
Wen Chun Tang <crptopochari@gmail.com>
2022-05-07 08:54:06 +0800

    update dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,23 @@
+foods = {'ramen':"拉麵", "pasta":"義大利麵"}
+foods = {
+	'ramen':"拉麵", 
+	"pasta":"義大利麵"
+}
+
+# Add key should with = value
+foods['tea'] = "茶"
+
+print(foods["ramen"])
+
+
+drinks1 = {
+	"name": "麥香奶茶",
+	"price": 10
+}
+drinks2 = {
+	"name": "奶茶",
+	"price": 30
+}
+drinks_menu = [drinks1, drinks2]
+drinks_menu[0]['name'] # "麥香奶茶"
+drinks_menu[1]['price'] # "30"
