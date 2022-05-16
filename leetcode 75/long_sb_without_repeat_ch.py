9050964221f1221ffd72074fdfca10258a032270
Wen Chun Tang <crptopochari@gmail.com>
2022-05-16 11:05:49 +0800

    refactor

--- leetcode 75/long_sb_without_repeat_ch.py
+++ leetcode 75/long_sb_without_repeat_ch.py
@@ -3,11 +3,10 @@ class Solution:
         window = set()
         lft_i = 0
         res = 0
-        for rgt_i in range(len(s)):
-            if s[rgt_i] not in window:
-                window.add s[rgt_i]
-                res = max( res, rgt_i - lft_i + 1)
+        for rgt_i in range(len(s)):               
             while s[rgt_i] in  window:
                 window.remove(s[lft_i])
                 lft_i += 1
+            window.add s[rgt_i]
+            res = max( res, rgt_i - lft_i + 1)
         return res
